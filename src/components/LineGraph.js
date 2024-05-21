d3652f56271421c7663c7a56faf00f1bec08b9f6
Vansh Kalra <vanshkalra645@gmail.com>
2024-05-21 23:44:46 +0530

    Task 2 - Part 1 completed.

---
+++ src/components/LineGraph.js
@@ -0,0 +1,72 @@
+// LineChart.js
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale
+);
+
+const LineGraph = ({ data }) => {
+  // console.log(data);
+console.log(data)
+  const chartData = {
+    labels: [2020, 2021, 2022, 2023, 2024],
+    datasets: [
+      {
+        data: data.chartData,
+        fill: false,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: data.mainTitle,
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: data.xTitleText,
+        },
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: data.yTitleText,
+        },
+      },
+    },
+  };
+
+  return <Line data={chartData} options={options} />;
+};
+
+export default LineGraph;
--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Table } from "antd";
+import LineGraph from "./LineGraph";
 
 export default function MainTable({ data }) {
-  const [tableData, setTableData] = useState([
-    { year: 0, jobCount: 0, averageSalary: 0 },
-  ]);
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     const processData = (data) => {
@@ -25,11 +24,11 @@ export default function MainTable({ data }) {
         return {
           year: year,
           jobCount: count,
-          averageSalary: "$" + Math.floor(totalSalary / 100 / count) * 100,
+          averageSalary: Math.floor(totalSalary / 100 / count) * 100,
         };
       });
       const finalAnalysedData = analysedData.filter((record) => record.year);
-      console.log(finalAnalysedData);
+      // console.log(finalAnalysedData);
       setTableData(finalAnalysedData);
     };
 
@@ -38,6 +37,49 @@ export default function MainTable({ data }) {
 
   // console.log(tableData);
 
+  // DATA FOR LineGraph
+  const yearsArray = []
+  for (let i = 0; i < tableData.length; i++) {
+    const element = tableData[i].year;
+    yearsArray.push(element);
+  }
+  console.log(yearsArray);
+
+  const jobCountArray = []
+  for (let i = 0; i < tableData.length; i++) {
+    const element = tableData[i].jobCount;
+    jobCountArray.push(element);
+  }
+  console.log(jobCountArray);
+
+  const salaryArray = []
+  for (let i = 0; i < tableData.length; i++) {
+    const element = tableData[i].averageSalary;
+    salaryArray.push(element);
+  }
+  console.log(salaryArray);
+
+  const LineGraphData = {
+    yearsArray,
+    jobCountArray,
+    salaryArray
+  }
+  console.log(LineGraphData)
+
+  const jobChartData = {
+    chartData: jobCountArray,
+    xTitleText: 'Years',
+    yTitleText: 'Number Of Jobs',
+    mainTitle: 'Line Graph for Number of Jobs'
+  }
+  const salaryChartData = {
+    chartData: salaryArray,
+    xTitleText: 'Years',
+    yTitleText: 'Average Salary in USD',
+    mainTitle: 'Line Graph for Average Salary'
+  }
+
+  // DATA FOR TABLE
   const columns = [
     {
       title: "Year",
@@ -56,13 +98,26 @@ export default function MainTable({ data }) {
       dataIndex: "averageSalary",
       key: "salary",
       sorter: (a, b) => a.averageSalary - b.averageSalary,
-    },
+    }, 
+    // {
+    //   title: 'Action',
+    //   key: 'action',
+    //   render: (text, record) => <LineGraph />
+    // }
   ];
 
   return (
     <div className="main-table">
       Table Comp working and salary is: <h1>{data[0].salary}</h1>
-      <Table dataSource={tableData} columns={columns} title={() => 'Main Table'} bordered/>
+      <Table
+        dataSource={tableData}
+        columns={columns}
+        title={() => "Main Table"}
+        bordered
+      />
+      <LineGraph data={jobChartData}/>
+      <LineGraph data={salaryChartData}/>
+
     </div>
   );
 }
